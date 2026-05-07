0dba1d1f1e10db8235b4a74b0a28af8efa4ff33a
TJunior03 <taibojnr@gmail.com>
2026-05-07 21:52:45 +0530

    Finilizing defining admin panel and test the ORM

--- certificates/admin.py
+++ certificates/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Certificate, TransactionLog
 
-# Register your models here.
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ['certificate_id', 'student_name', 'course_name', 'issue_date', 'issuer', 'created_at']
+    search_fields = ['student_name', 'course_name', 'certificate_hash']
+    readonly_fields = ['certificate_id', 'certificate_hash', 'created_at']
+
+
+@admin.register(TransactionLog)
+class TransactionLogAdmin(admin.ModelAdmin):
+    list_display = ['certificate_hash', 'tx_hash', 'blockchain', 'timestamp']
+    readonly_fields = ['timestamp']
