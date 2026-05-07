f58282e69379dbc25df5c5ad9d74f14588e4cb0b
TJunior03 <taibojnr@gmail.com>
2026-05-07 22:39:26 +0530

    Implemented certificate issuance logic with SHA-256 hashing

---
+++ certificates/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Certificate
+
+
+class CertificateForm(forms.ModelForm):
+    class Meta:
+        model = Certificate
+        fields = ['student_name', 'course_name', 'issue_date']
--- certificates/views.py
+++ certificates/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CertificateForm
+from .models import Certificate
+from .utils import generate_certificate_hash
 
-# Create your views here.
+
+def issue_certificate(request):
+
+    if request.method == 'POST':
+
+        form = CertificateForm(request.POST)
+
+        if form.is_valid():
+
+            certificate = form.save(commit=False)
+
+            # Generate SHA-256 hash
+            cert_hash = generate_certificate_hash(
+                certificate.student_name,
+                certificate.course_name,
+                certificate.issue_date
+            )
+
+            certificate.certificate_hash = cert_hash
+
+            # Save issuer (logged-in admin)
+            if request.user.is_authenticated:
+                certificate.issuer = request.user
+
+            # Save to database
+            certificate.save()
+
+            return redirect('issue_certificate')
+
+    else:
+        form = CertificateForm()
+
+    return render(request, 'certificates/issue_certificate.html', {
+        'form': form
+    })
