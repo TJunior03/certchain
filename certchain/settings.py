7f207bb6364e8fbb610b3c10690690087fddb2f1
TJunior03 <taibojnr@gmail.com>
2026-05-11 20:07:11 +0530

    Phase 4 complete: Docker, docker-compose, Nginx, Gunicorn, containerized stack

--- certchain/settings.py
+++ certchain/settings.py
@@ -132,3 +132,9 @@ import os
 
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL  = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL  = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
